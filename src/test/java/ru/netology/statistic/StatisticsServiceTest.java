b92f12891935cece38a59b18750015da0e5f68a8
licoris <klepehin.k.a@gmail.com>
2026-01-17 20:44:40 +0300

    bug fix

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,6 +17,7 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void findMaxMid() {
         StatisticsService service = new StatisticsService();
